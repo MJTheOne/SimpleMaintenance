230bdbae3c167d07d9b3228e92c15ec953f05324
Marius <mjtheone@gmail.com>
2016-12-24 20:54:15 +0100

    typo: extends to implements

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -11,7 +11,7 @@ namespace CoderGeek\Bundle\MaintenanceBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-class Configuration extends ConfigurationInterface
+class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
